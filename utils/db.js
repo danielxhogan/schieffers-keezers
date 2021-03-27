e42a3dfde20ab1fd658bdc862829e99a142f4e6a
rando_mane <hogand@sou.edu>
2021-03-27 13:20:20 -0700

    modify db connection

--- utils/db.js
+++ utils/db.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 const Pool = require('pg').Pool;
 
 const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE
+  user: 'rando_mane',
+  password: 'COLExBURNED#363',
+  host: 'localhost',
+  port: 5432,
+  database: 'sk'
 };
 
 const proConfig = {
