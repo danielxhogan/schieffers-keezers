5b577cb29238d1162c9e048501e7b486fd0c5d7a
rando_mane <hogand@sou.edu>
2021-03-28 10:07:17 -0700

    connect to rds

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,18 @@ require('dotenv').config()
 
 const Pool = require('pg').Pool;
 
+// const devConfig = {
+//   user: 'rando_mane',
+//   password: process.env.PG_PASSWORD,
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'sk'
+// };
+
 const devConfig = {
-  user: 'rando_mane',
-  password: process.env.PG_PASSWORD,
-  host: 'localhost',
+  user: 'postgres',
+  password: 'COLExBURNED#363',
+  host: 'database-2.c7stlrulp8px.us-east-1.rds.amazonaws.com',
   port: 5432,
   database: 'sk'
 };
@@ -17,6 +25,7 @@ const proConfig = {
   }
 };
 
-const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devConfig);
+// const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devConfig);
+const pool = new Pool(devConfig);
 
 module.exports = pool;
