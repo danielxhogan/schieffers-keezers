6cac9de7153300db29d651451f46db00ad9fd052
rando_mane <hogand@sou.edu>
2021-05-17 10:51:53 -0700

    update user/host for db connection

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
-
 const Pool = require('pg').Pool;
 
-const pool = new Pool({
-  user: 'rando_mane',
-  password: process.env.PG_PASSWORD,
-  host: 'localhost',
-  port: 5432,
-  database: 'sk'
-});
+try {
+  const pool = new Pool({
+    user: 'root',
+    password: process.env.PG_PASSWORD,
+    host: 'localhost',
+    port: 5432,
+    database: 'sk'
+  });
+
+} catch (err) {
+  console.log(err.message);
+}
+
 
 module.exports = pool;
