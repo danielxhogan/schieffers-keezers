a4ec9e2f3018c35fe2e9018699fe1f9280f73543
rando_mane <hogand@sou.edu>
2021-05-17 10:37:17 -0700

    use local db

--- utils/db.js
+++ utils/db.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: 'postgres',
+  user: 'rando_mane',
   password: process.env.PG_PASSWORD,
-  host: 'database-2.c7stlrulp8px.us-east-1.rds.amazonaws.com',
+  host: 'localhost',
   port: 5432,
   database: 'sk'
 });
