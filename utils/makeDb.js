975e45fee898b1b6e35d363443e03f74a83c426b
danielxhogan <danielxhogan@gmail.com>
2022-12-24 16:52:52 -0800

    initial db reverse engineer

--- utils/makeDb.js
+++ utils/makeDb.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const Pool = require('pg').Pool;
 
-console.log(process.env.PG_PASSWORD)
-
 try {
   const pool = new Pool({
     user: 'postgres',
@@ -12,9 +10,51 @@ try {
     database: 'sk'
   });
 
-console.log(process.env.PG_PASSWORD)
 
-  pool.query('create table if not exists test_table (first_column text, second_column integer)');
+  pool.query('create table if not exists users(\
+    user_id int primary key\
+    first_name text\
+    last_name text\
+    email text\
+    password text\
+    )');
+
+  pool.query('create table if not exists addresses(\
+    address_id int primary key\
+    street text\
+    city text\
+    state text\
+    zip int\
+    user_id int references users(user_id)\
+    )');
+
+
+  pool.query('create table if not exists categories(\
+    category_id int primary key\
+    name text\
+  )');
+
+  pool.query('create table if not exists products(\
+    product_id int primary key\
+    name text\
+    description text\
+    price numeric\
+    category_id int references categories(category_id)\
+  )');
+
+  pool.query('create table if not exists images(\
+    image_id int primary key\
+    name text\
+    product_id int references products(product_id)\
+    )');
+
+
+  pool.query('create table if not exists cart(\
+    cart_item_id int primary key\
+    user_id int refrences users(user_id)\
+    product_id int references products(product_id)\
+    qty\
+    )');
 
   pool.end();
 
